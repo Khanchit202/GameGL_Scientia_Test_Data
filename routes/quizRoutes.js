00ef7d2fe937740e06be973567816ba18e048e5c
Khanchit202 <khanchit202@gmail.com>
2025-03-13 06:31:22 +0700

    9999

--- routes/quizRoutes.js
+++ routes/quizRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Quiz = require('../models/Quiz');
+const User = require('../models/User');
 const auth = require('../middleware/auth');
 
 // @route   POST api/quiz/add
